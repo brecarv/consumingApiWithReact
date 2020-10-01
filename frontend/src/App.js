bc5dd8b0d8af0989ae9c07e105e955fbb4db0a43
Emerson Carvalho <brecarv@outlook.com>
2020-10-01 09:28:18 -0300

    Consumindo artigos p/ teste

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,29 +8,33 @@ function App() {
   const ListLoading = withListLoading(List);
   const [appState, setAppState] = useState({
     loading: false,
-    repos: null,
+    artigos: null,
   });
 
   useEffect(() => {
     setAppState({ loading: true });
-    const apiUrl = `https://api.github.com/users/brecarv/repos`;
+    //const apiUrl = `https://api.github.com/users/brecarv/`;
+    //
+    
+    const apiUrl = `https://carnvnode.herokuapp.com/api/artigos`;
+    //const apiUrl = `http://localhost:3000/api/artigos/`
     /* fetch(apiUrl)
     .then((res) => res.json())
-    .then((repos) => {
-    setAppState({ loading: false, repos: repos });
+    .then((artigos) => {
+    setAppState({ loading: false, artigos: artigos });
     }); */
-    axios.get(apiUrl).then((repos) => {
-      const allRepos = repos.data;
-      setAppState({loading: false, repos: allRepos});
+    axios.get(apiUrl).then((artigos) => {
+      const todosArtigos = artigos.data.artigos;
+      setAppState({loading: false, artigos: todosArtigos});
     });
   }, [setAppState]);
   return (
     <div className='App'>
       <div className='container'>
-        <h1>My Repositories</h1>
+        <h1>Artigos</h1>
       </div>
-      <div className='repo-container'>
-        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      <div className='artigo-container'>
+        <ListLoading isLoading={appState.loading} artigos={appState.artigos} />
       </div>
       <footer>
         <div className='footer'>
--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -1,15 +1,15 @@
 import React from 'react';
 const List = (props) => {
-  const { repos } = props;
-  if (!repos || repos.length === 0) return <p>No repos, sorry</p>;
+  const { artigos } = props;
+  if (!artigos || artigos.length === 0) return <p>Sem artigos, sorry!</p>;
   return (
     <ul>
-      <h2 className='list-head'>Available Public Repositories</h2>
-      {repos.map((repo) => {
+      <h2 className='list-head'>Artigos Cadastrados</h2>
+      {artigos.map((artigo) => {
         return (
-          <li key={repo.id} className='list'>
-            <span className='repo-text'>{repo.name} </span>
-            <span className='repo-description'>{repo.description}</span>
+          <li key={artigo.slug} className='list'>
+            <span className='artigo-text'>{artigo.titulo} </span>
+            <span className='artigo-description'>{artigo.descricao}</span>
           </li>
         );
       })}
